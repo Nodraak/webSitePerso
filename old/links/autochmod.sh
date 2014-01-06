1d89292c3554934a24f1f4fefc4fcdb88e18a3c9
Adrien Chardon <adrienchardon@mailoo.org>
2014-01-06 23:42:00 +0100

    deleted old dir

--- old/links/autochmod.sh
+++
@@ -1,8 +0,0 @@
-# directories first
-find * -type d | xargs chmod 771 # 755 ?
-# all files first
-find * -type f | xargs chmod 664
-# php files then
-find *.php* -type f | xargs chmod 770
-echo "autochmod : done changing permissions for folders below that one"
-echo "autochmod : you now should have a working web page"
