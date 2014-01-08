0a2d57a85c132900fe17582d122f9d82516e6251
Adrien Chardon <adrienchardon@mailoo.org>
2014-01-08 20:43:22 +0100

    removed unused files

--- chat/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-	<p>Page privée mais code public, bientôt dispo sur un repo <a href="http://github.com/Nodraak">git</a></p>
-</footer>
--- chat/head.php
+++
@@ -1,5 +0,0 @@
-
-<meta charset="utf-8" />
-<!--<link rel="icon" type="image/png" href="favicon.png">-->
-<!--<link rel="stylesheet" href="styles.css" />-->
-
