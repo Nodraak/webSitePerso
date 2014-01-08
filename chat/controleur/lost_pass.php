ba9dac8f2d271b08537d23d6298fa4d16a166abb
Adrien Chardon <adrienchardon@mailoo.org>
2014-01-08 21:53:38 +0100

    added lost_pass.php page

---
+++ chat/controleur/lost_pass.php
@@ -0,0 +1,10 @@
+
+<?php
+
+	echo '<h2>T\'as perdu ton mot magique ?</h2>';
+
+	echo '<p>T\'es vraiment trop mauvais(e) ! (enfin, si t\'es blond(e), on te pardonne).<br />
+				T\'as plus qu\'a envoyer un mail a l\'admin : adrien.duranquet_AT_laposte_POINT_net en indiquant ton pseudo et ton mail.</p>';
+
+?>
+
