4f7041742273a8ab03082ebc0b4b12bf0cbabfc9
Adrien Chardon <adrienchardon@mailoo.org>
2014-01-11 20:59:37 +0100

    updated footer

--- chat/vue/footer.php
+++ chat/vue/footer.php
@@ -2,7 +2,10 @@
 <footer>
 	<div class="left"></div>
 	<div class="middle center">
-		<p>Page perso, mais code public, dispo sur mon <a href="https://github.com/Nodraak/webSitePerso/tree/master/chat">github</a>.</p>
+		<p>
+			Conception et réalisation Adrien Chardon.<br />
+			Page perso, mais code public, dispo sur mon <a href="https://github.com/Nodraak/webSitePerso/tree/master/chat">github</a>.
+		</p>
 	</div>
 	<div class="right"></div>
 </footer>
