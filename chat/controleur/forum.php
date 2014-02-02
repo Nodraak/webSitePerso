80a83f48224c54f6b557103bd8b837a8ef2dbd2a
Adrien Chardon <adrienchardon@mailoo.org>
2014-02-02 19:11:36 +0100

    added link to acces last page of a thread

--- chat/controleur/forum.php
+++ chat/controleur/forum.php
@@ -64,10 +64,13 @@
 		$forum = new Forum($data_thread['id']);
 		$author = new Membre($forum->get_owner());
 
+		$urlFirstPage = 'index.php?page=message&id='.$forum->get_id().'&offset=1';
+		$urlLastPage = 'index.php?page=message&id='.$forum->get_id().'&offset=last';
+
 		echo '<div class="thread"><table>
 				<tr>
-					<td class="title"><a href="index.php?page=message&id='.$forum->get_id().'&offset=1">'.$forum->get_title().'</a></td>
-					<td class="time time_activity">'.$forum->get_nbMessage().' messages, dernière activité le ' . date('d/m/Y à H\hi', $forum->get_activity()) . '</td>
+					<td class="title"><a href="'.$urlFirstPage.'">'.$forum->get_title().'</a></td>
+					<td class="time time_activity">'.$forum->get_nbMessage().' messages, <a href="'.$urlLastPage.'">dernière activité</a> le ' . date('d/m/Y à H\hi', $forum->get_activity()) . '</td>
 				</tr><tr>
 					<td class="auteur"><a href="index.php?page=user&id='.$forum->get_owner().'">'.$author->get_pseudo().'</a></td>
 					<td class="time time_created">Créé le ' . date('d/m/Y à H\hi', $forum->get_created()) . '</td>
